a8737b7258ecaf593ad90440973b9a33bfbc7f0f
Youngjae <2015fromjeki@gmail.com>
2024-09-11 08:05:22 +1000

    Add Rating Color Function

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const MovieCard = ({ movie }) => {
   const handleError = (e) => {
     e.target.src = "../../public/images/default.jpg";
   };
+  const [rate, setRate] = useState("rating-ok");
+  // rating function
+
+  const rating = (rating) => {
+    if (rating < 5) {
+      setRate("rating-bad");
+    } else if (rating >= 5 && rating <= 7) {
+      setRate("rating-ok");
+    } else if (rating >= 7) {
+      setRate("rating-good");
+    }
+  };
+  useEffect(() => {
+    rating(movie.rating);
+  }, []);
 
   return (
     <div className="movie-card" key={movie.id}>
@@ -14,7 +29,7 @@ const MovieCard = ({ movie }) => {
       />
       <div className="movie-card-info">
         <p className="movie-card-title">{movie.title}</p>
-        <p className="movie-card-rating">{movie.rating}</p>
+        <p className={`movie-card-rating ${rate}`}>{movie.rating}</p>
         <p className="movie-card-genre">{movie.genre}</p>
       </div>
     </div>
