32c7c7f94138ce641725f9d2d1961c59c50dcd5d
Youngjae <2015fromjeki@gmail.com>
2024-09-11 10:01:39 +1000

    Add Filtering Function

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -3,17 +3,44 @@ import movieCollections from "../../public/movies.json";
 import MovieCard from "./MovieCard";
 const MoviesGrid = () => {
   const [moives, setMoives] = useState([]);
+  const [filteredMovie, setFilteredMovie] = useState([]);
+  const [searchingMovieTitle, setSearchingMovieTitle] = useState("");
 
   useEffect(() => {
     setMoives(movieCollections);
+    // Keep the original files
+    setFilteredMovie(movieCollections);
   }, []);
 
+  // Searching Function
+  const filterMovie = (title) => {
+    if (title.trim() === "") {
+      setFilteredMovie(moives);
+    } else {
+      const targetMovie = filteredMovie.filter((movie) =>
+        movie.title.toLowerCase().trim().includes(title.toLowerCase().trim())
+      );
+      setFilteredMovie(targetMovie);
+    }
+  };
+
+  // Testing console
+  console.log(searchingMovieTitle);
+  console.log(filteredMovie);
   return (
-    <div className="movies-grid">
-      {moives.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
+    <>
+      <input
+        type="text"
+        placeholder="Search movie..."
+        className="search-input"
+        onChange={(e) => filterMovie(e.target.value)}
+      />
+      <div className="movies-grid">
+        {filteredMovie.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </>
   );
 };
 
