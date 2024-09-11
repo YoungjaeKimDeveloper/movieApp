32ae86ffa5af8f34099d6f1177120b1d6bcbe47d
Youngjae <2015fromjeki@gmail.com>
2024-09-11 16:55:32 +1000

    Add Drop UI

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -6,6 +6,9 @@ const MoviesGrid = () => {
   const [filteredMovie, setFilteredMovie] = useState([]);
   const [searchingMovieTitle, setSearchingMovieTitle] = useState("");
 
+  const [genre, setGenre] = useState("All genres");
+  const [rating, setRating] = useState("All");
+
   useEffect(() => {
     setMoives(movieCollections);
     // Keep the original files
@@ -35,6 +38,26 @@ const MoviesGrid = () => {
         className="search-input"
         onChange={(e) => filterMovie(e.target.value)}
       />
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>All Genre</label>
+          <select className="filter-dropdown">
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+            <option>Action</option>
+          </select>
+        </div>
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select className="filter-dropdown">
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
+      </div>
       <div className="movies-grid">
         {filteredMovie.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
